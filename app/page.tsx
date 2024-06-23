d35e67cba67f62ceff78b5513c1317d638e4326a
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-06-23 12:52:15 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,158 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Badge } from "@/components/ui/badge";
+import { GitHubLogoIcon, InstagramLogoIcon, ResetIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Progress } from "@/components/ui/progress";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner"
 
 export default function Home() {
+  const router = useRouter();
+
+  const [progress, setProgress] = useState(1);
+  const [isSecondDialogOpen, setIsSecondDialogOpen] = useState(false);
+  const [isOpened, setIsOpened] = useState(false);
+
+  const handleOpen = () => {
+    const opened = localStorage.getItem('isOpened') === 'true';
+    setIsOpened(opened);
+    console.log('isOpened:', opened);
+
+    if (!opened) {
+      const duration = 8000;
+      const interval = duration / 100;
+
+      const timer = setInterval(() => {
+        setProgress(prevProgress => {
+          if (prevProgress >= 100) {
+            clearInterval(timer);
+            setIsSecondDialogOpen(true);
+            return 100;
+          }
+          return prevProgress + 1;
+        });
+      }, interval);
+
+      return () => clearInterval(timer);
+    }
+  };
+
+  useEffect(() => {
+    if (isSecondDialogOpen) {
+      localStorage.setItem('isOpened', 'true');
+    }
+  }, [isSecondDialogOpen]);
+
+  const handleReset = () => {
+    localStorage.setItem('isOpened', 'false');
+    setIsOpened(false);
+    toast("Values are being reset.")
+  }
+
+  const handleCancel = () => {
+    setProgress(1);
+    setIsSecondDialogOpen(false);
+    const opened = localStorage.getItem('isOpened') === 'true';
+    setIsOpened(opened);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 backgroundImg">
+
+      <Button variant='link' className="absolute h-8 top-4 right-4 flex flex-row gap-2" onClick={handleReset}>
+        <span>Reset</span>
+        <div className="w-auto h-auto flex items-center justify-center rounded-full bg-black p-1">
+          <ResetIcon className="w-4 h-4 text-white" />
         </div>
+      </Button>
+
+      {!isSecondDialogOpen && (
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <button className="button-11" role="button" onClick={handleOpen}>
+              <div className="button-11__content">
+                <div className="button-11__icon">
+                  <Image src='/peace.png' alt="" width={40} height={40} />
+                </div>
+                <p className="button-11__text text">Make Everything OK</p>
+              </div>
+            </button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle className="flex flex-col">
+                {!isOpened ? (
+                  <>
+                    <span>Making everything OK is in progress.</span>
+                    <span className="mb-4 text-sm font-normal">Please wait for some time while we make everything OK.</span>
+                  </>
+                ) : (
+                  <>
+                    <span>Everything is already fine.</span>
+                    <span className="mb-4 text-sm font-normal">Don't stress too much, life is too short to take stress. Wanna talk, we're here for you.</span></>
+                )}
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                {!isOpened && <Progress value={progress} className="mb-4" />}
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              {!isOpened ? (
+                <AlertDialogCancel className="h-8" onClick={handleCancel}>still wanna stop!?</AlertDialogCancel>
+              ) : (
+                <AlertDialogCancel className="h-8" onClick={() => router.push('https://wa.me/7004817946')}>We're here!</AlertDialogCancel>
+              )}
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      )}
+
+      {isSecondDialogOpen && (
+        <AlertDialog open={true}>
+          <AlertDialogContent className="bg-[#009568] border-[5px] border-[#056548] rounded-sm flex flex-col items-center justify-center text-center p-10">
+            <AlertDialogHeader>
+              <AlertDialogTitle className="text-2xl font-bold text-white text-center">Everything is OK now</AlertDialogTitle>
+              <AlertDialogDescription className="text-lg font-medium text-white text-center">
+                If everything is still not OK,
+                try checking your settings of perception of objective reality.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogAction onClick={() => { setIsSecondDialogOpen(false); setProgress(100) }} className="h-10 text-[#056548] bg-white border-4 border-[#056548] hover:bg-gray-100">Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      )}
+
+      <div className="absolute bottom-4 flex flex-row gap-2">
+        <Badge variant="default" className="flex flex-row gap-2 rounded-sm py-1">
+          <GitHubLogoIcon className="w-4 h-4" />
+          <span>Github</span>
+        </Badge>
+        <Badge variant="default" className="flex flex-row gap-2 rounded-sm py-1">
+          <InstagramLogoIcon className="w-4 h-4" />
+          <span>Instagram</span>
+        </Badge>
+        <Badge variant="default" className="flex flex-row gap-2 rounded-sm py-1">
+          <TwitterLogoIcon className="w-4 h-4" />
+          <span>X</span>
+        </Badge>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
